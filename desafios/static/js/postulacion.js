94e1175d950a62fcacd261ee135195295dc47cbf
Benjamin Muñoz <benjamin.munoz8914@gmail.com>
2025-06-09 20:35:58 -0400

    Delete desafios directory

--- desafios/static/js/postulacion.js
+++
@@ -1,7 +0,0 @@
-const viewMoreText = document.querySelector('.view-more');
-const textContent = document.querySelector('.text');
-
-viewMoreText.addEventListener('click', () => {
-    textContent.classList.toggle('expandido');
-    viewMoreText.textContent = textContent.classList.contains('expandido') ? 'Ver menos' : 'Ver más';
-});
