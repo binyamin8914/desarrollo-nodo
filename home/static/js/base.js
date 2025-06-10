91f80ce6879afedbbc5f2a9c42b8b80088f5dbdf
Benjamin Muñoz <benjamin.munoz8914@gmail.com>
2025-06-09 20:36:04 -0400

    Delete home directory

--- home/static/js/base.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const sidebar = document.getElementById('sidebar');
-
-menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
-sidebar.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-});
--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('detalle/<int:id>/', views.detalle, name='detalle'),
-    path('contacto/', views.contacto, name='contacto'),
-    path('postula/', views.postula, name='postula'),
-]
